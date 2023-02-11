181ffe6ac41a0bf557ecdfc7c8a4ef6db6ae85fd
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 13:16:39 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ScreensA from './src/screens/ScreensA';
-import Routes from './src/routes/index';
+import Routes from './src/routes/index.tsx';
 
 export default function App() {
   return (
