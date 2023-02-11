be1742ed35547ba28372f1056be2665643350479
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 12:00:45 +0200

    Create ScreensB.tsx

---
+++ src/screens/ScreensB.tsx
@@ -0,0 +1,11 @@
+import  React  from 'react';
+import { View } from 'react-native';
+
+export default function ScreensB() {
+    return (
+        <View>
+            ScreensB
+        </View>
+    )
+
+}
