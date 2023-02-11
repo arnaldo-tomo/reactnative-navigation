c1bcbe503a0a753a16e1495a77cc89dc6f69107a
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 12:00:44 +0200

    Create ScreensA.tsx

---
+++ src/screens/ScreensA.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function ScreensA() {
+    return (
+        <View>
+          ScreensA
+        </View>
+    )
+    
+}
