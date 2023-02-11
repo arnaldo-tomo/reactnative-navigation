574ae1f682968fb022aed96499bf899c6c017556
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 13:52:34 +0200

    Update ScreensA.tsx

--- src/screens/ScreensA.tsx
+++ src/screens/ScreensA.tsx
@@ -1,10 +1,22 @@
-import { View,Button } from "react-native";
+import { View,Button,Text } from "react-native";
 
 export default function ScreensA({ navigation}) {
     return (
-        <View>
-        <Button  title="Update the title" onPress={() => navigation.push('Details')}/>
-        </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        
+        <Text>Home Screen</Text>
+        
+        <Button title="com paramentrp" onPress={() => navigation.navigate('Details',{ idade: 'jane', nome:'ARrnaldo tomo' } )} />
+        
+        <Text>Segunda Forma</Text>
+
+        <Button
+  title="Go to Details... again"
+  onPress={() => navigation.push('Details')}
+        />
+        
+        
+    </View>
     )
     
 }
