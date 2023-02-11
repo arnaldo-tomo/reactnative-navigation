6c298aeb14d1e9c9130de21afd265b3fc43c7f3f
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 14:16:07 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,63 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ScreensA from '../screens/ScreensA';
 import ScreensB from '../screens/ScreensB';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Tab = createBottomTabNavigator();
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
-  return (
+return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={ScreensA} options={{ title: 'Inicio' }} />
-        <Stack.Screen name="Details" component={ScreensB} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            } else {
+               iconName = focused ? 'ios-filter' : 'ios-filter-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Detalis" component={ScreensA} />
+        <Tab.Screen name="Settings" component={ScreensB} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
+
+
+
 }
+//navigation Simples
+// return (
+//   <NavigationContainer>
+//     <Stack.Navigator initialRouteName="Home">
+//       <Stack.Screen name="Home" component={ScreensA} options={{ title: 'Inicio' }} />
+//       <Stack.Screen name="Details" component={ScreensB} />
+//     </Stack.Navigator>
+//   </NavigationContainer>
+// );
