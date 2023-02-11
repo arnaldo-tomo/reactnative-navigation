b1bc4844f37cfbb04d22869269c2df0c58c9d774
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 14:16:12 +0200

    Update ScreensB.tsx

--- src/screens/ScreensB.tsx
+++ src/screens/ScreensB.tsx
@@ -1,12 +1,11 @@
 import {React}  from 'react';
 import { View,Button,Text } from 'react-native';
 
-export default function ScreensB({ navigation,route }) {
-    let { idade,nome } = route.params;
+export default function ScreensB({ navigation}) {
+    // let { idade,nome } = route.params;
     return (
-        <View>
-            <Text>{idade}</Text>
-            <Text>{nome}</Text>
+           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        
             <Button title="Go back" onPress={() => navigation.goBack()} />
         </View>
     )
