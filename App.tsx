e450f854f3e5716a7dca90fabed5c369743ecd47
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-11 12:00:29 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ScreensA from './src/screens/ScreensA';
+
+export default function App() {
+  return (
+    <ScreensA/>
+  );
+}
+
